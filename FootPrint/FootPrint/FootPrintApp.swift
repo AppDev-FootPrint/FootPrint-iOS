9fa185147ffce5e1c9e146d15d17d70fc4cfa150
dayo2n <0217dayun@naver.com>
2022-04-27 21:45:22 +0900

    initial setting

---
+++ FootPrint/FootPrint/FootPrintApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootPrintApp.swift
+//  FootPrint
+//
+//  Created by 문다 on 2022/04/27.
+//
+
+import SwiftUI
+
+@main
+struct FootPrintApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
