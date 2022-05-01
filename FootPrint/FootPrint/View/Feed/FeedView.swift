64954d2021c3aea11cf6541b48326871d29482ba
dayo2n <0217dayun@naver.com>
2022-05-01 19:48:52 +0900

    [init] 개발 환경 구축

---
+++ FootPrint/FootPrint/View/Feed/FeedView.swift
@@ -0,0 +1,20 @@
+//
+//  FeedView.swift
+//  FootPrint
+//
+//  Created by 문다 on 2022/05/01.
+//
+
+import SwiftUI
+
+struct FeedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FeedView_Previews: PreviewProvider {
+    static var previews: some View {
+        FeedView()
+    }
+}
---
+++ FootPrint/FootPrint/View/Main/MainTabView.swift
@@ -0,0 +1,55 @@
+//
+//  MainTabView.swift
+//  FootPrint
+//
+//  Created by 문다 on 2022/05/01.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        NavigationView {
+            TabView {
+                FeedView()
+                    .tabItem {
+                        Image(systemName: "house")
+                    }
+                
+                SearchView()
+                    .tabItem {
+                        Image(systemName: "magnifyingglass")
+                    }
+                
+                CreateTravel()
+                    .tabItem {
+                        Image(systemName: "plus.square")
+                    }
+                
+                NotificationView()
+                    .tabItem {
+                        Image(systemName: "heart")
+                    }
+                
+                ProfileView()
+                    .tabItem {
+                        Image(systemName: "person")
+                    }
+            }
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .principal) {
+                    Image("logo")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                }
+            }
+        }
+    }
+}
+
+struct MainTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainTabView()
+    }
+}
