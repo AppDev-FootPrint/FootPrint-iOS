2bbb5b4e91d7a968f6b3808f576ec057b08803f3
dayo2n <0217dayun@naver.com>
2022-05-17 22:09:57 +0900

    [feat] create cell for price #3

---
+++ FootPrint/FootPrint/View/Travel/PriceCell.swift
@@ -0,0 +1,37 @@
+//
+//  PriceCell.swift
+//  FootPrint
+//
+//  Created by 문다 on 2022/05/17.
+//
+
+import SwiftUI
+
+struct PriceCell: View {
+    
+    @State private var isEdited : Bool = true
+    @State private var content : String = ""
+    @State private var price : String = ""
+    
+    // true: 글 작성 중의 가격셀, false : 게시글 조회 중의 가격 셀
+    var body: some View {
+        HStack {
+            TextField("contents...", text: $content)
+                .padding(10)
+                .border(.black, width: 1)
+                .padding(.leading)
+            
+            TextField("price...", text: $price)
+                .frame(width: 100)
+                .padding(10)
+                .border(.black, width: 1)
+                .padding(.trailing)
+        }
+    }
+}
+
+struct PriceCell_Previews: PreviewProvider {
+    static var previews: some View {
+        PriceCell()
+    }
+}
