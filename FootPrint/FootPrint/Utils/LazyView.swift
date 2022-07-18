24a3a3bfe30c388bc3d473babf63ebdcd25fae88
dayo2n <0217dayun@naver.com>
2022-07-18 15:57:32 +0900

    [feat] Lazy loading view 구현

--- FootPrint/FootPrint/Utils/LazyView.swift
+++ FootPrint/FootPrint/Utils/LazyView.swift
@@ -5,4 +5,15 @@
 //  Created by 문다 on 2022/07/18.
 //
 
-import Foundation
+import SwiftUI
+
+struct LazyView<Content: View>: View {
+    let build: () -> Content
+    init(_ build: @autoclosure @escaping() -> Content) {
+        self.build = build
+    }
+    
+    var body: Content {
+        build()
+    }
+}
