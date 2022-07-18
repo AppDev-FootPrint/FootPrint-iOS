fc695d7154630bd92bceb0dbd47e0cfc58c15e15
dayo2n <0217dayun@naver.com>
2022-07-18 16:27:28 +0900

    [fix] SearchListView 개발범위에서 제외

--- FootPrint/FootPrint/View/Items/SearchListView.swift
+++ FootPrint/FootPrint/View/Items/SearchListView.swift
@@ -7,24 +7,25 @@
 
 import SwiftUI
 
+// 기간 내 구현되지 않을 예정인 부분
 struct SearchListView: View {
     
     @ObservedObject var viewModel: SearchViewModel
     @Binding var searchText: String
     
-    var users: [User] {
-        return searchText.isEmpty ? viewModel.users : viewModel.filteredUsers(searchText)
+    var travels: [MainTravel] {
+        return searchText.isEmpty ? viewModel.travels : viewModel.filteredHistories(searchText)
     }
     
     var body: some View {
         ScrollView {
             LazyVStack {
-                ForEach(users) { user in
+                ForEach(self.travels) { travel in
                     NavigationLink (
-                        destination: LazyView(ProfileView(user: user)),
+                        destination: EmptyView(),
                         label: {
-                            UserCell(user: user)
-                                .padding(.leading)
+//                            UserCell(user: user)
+//                                .padding(.leading)
                     })
                 }
             }
