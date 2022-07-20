4888ea63be2419d43846028c4ea5a5bdcd544773
dayo2n <0217dayun@naver.com>
2022-07-20 16:14:18 +0900

    [feat] String (2000-00-00 format) to Date

--- FootPrint/FootPrint/Utils/DateConverter.swift
+++ FootPrint/FootPrint/Utils/DateConverter.swift
@@ -20,3 +20,13 @@ func Date2String(date: Date) -> String {
     
     return "\(splitOnlyDate[0])"
 }
+
+// "2000-00-00" 형식의 문자열을 "2000년 0월 0일의 00:00:00"의 Date 타입으로 리턴
+func String2Date(date: String) -> Date {
+    let splitDate = date.split(separator: "-")
+    let stringDate = "\(splitDate[0])-\(splitDate[1])-\(splitDate[2]) 00:00:00"
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+    dateFormatter.timeZone = TimeZone(identifier: "KST")
+    return dateFormatter.date(from: stringDate) ?? Date()
+}
